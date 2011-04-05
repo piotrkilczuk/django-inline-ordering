8d7799585890a3561b1ab8913d4524d2b85d0f5b
centralniak <p.kilczuk@stylesheet.pl>
2011-04-05 16:20:08 +0200

    Reorganized media to use Django 1.3 directory layout

---
+++ inline_ordering/static/inline_ordering.js
@@ -0,0 +1,49 @@
+var InlineOrdering = {
+
+    /**
+     * Get list of elements that can be reordered
+     *
+     * At this point, only already existent records can be reordered (ie. where pk != '')
+     *
+     * @return Array
+     * @todo Check if given record changed, and if so, make it reorderable
+     * @todo Primary key might not be 'id' - better selector needed
+     *
+     */
+    getOrderables: function(){
+        return jQuery('div.inline-group span.delete').parents('.inline-related');
+    },
+    
+    /**
+     * Inits the jQuery UI D&D
+     *
+     */
+    init: function(){
+        jQuery("div.inline-group").sortable({
+            axis: 'y',
+            placeholder: 'ui-state-highlight',
+            forcePlaceholderSize: 'true',
+            items: InlineOrdering.getOrderables(),
+            update: InlineOrdering.update
+        });
+        jQuery("div.inline-group").disableSelection();
+        
+        jQuery(this).find('div.inline_ordering_position').hide();
+        jQuery('.add-row a').click(InlineOrdering.update);
+    },
+    
+    /**
+     * Updates the position field
+     *
+     */
+    update: function(){
+        InlineOrdering.getOrderables().each(function(i){
+            jQuery(this).find('input[id$=inline_ordering_position]').val(i + 1);
+        });
+    }
+    
+};
+
+jQuery(function(){
+    InlineOrdering.init();
+});
